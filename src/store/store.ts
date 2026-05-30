cd676039f448d830dbc75a6cf6ba649203a9c41d
OwenLobato <owenlobatov@gmail.com>
2026-05-29 22:02:25 -0600

    fix(store): disable serializableCheck to allow Date objects in Redux state
    - pass serializableCheck: false to getDefaultMiddleware
    - calendar events hold Date values (start, end) which are non-serializable by default

--- src/store/store.ts
+++ src/store/store.ts
@@ -7,4 +7,8 @@ export const store = configureStore({
     ui: uiSlice.reducer,
   },
   devTools: true,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
